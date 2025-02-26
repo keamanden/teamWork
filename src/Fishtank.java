cff2324ea3e3c38ee1cf24e932ded4d68b262436
MikkelGodiksen <Mikk6326@stud.kea.dk>
2025-02-26 11:38:11 +0100

    inden afsluting

--- src/Fishtank.java
+++ src/Fishtank.java
@@ -1,2 +1,36 @@
+import java.time.LocalDate;
+
 public class Fishtank {
+private LocalDate lastWaterChange;
+private WaterQuality waterQuality;
+private String waterChangeNote;
+
+
+//Definere de mulige status værdier for vandet uden at man kan indtaste en forkert værdi
+public enum WaterQuality{
+    PERFECT, GOOD, OKAY, BAD
+}
+
+public Fishtank(){
+    this.lastWaterChange = LocalDate.now();
+    this.waterQuality = WaterQuality.GOOD;
+    this.waterChangeNote = "Standard note";
+
+}
+
+    public LocalDate getLastWaterChange(){
+    return lastWaterChange;
+    }
+
+    public WaterQuality getWaterQuality(){
+        return waterQuality;
+    }
+    public String getWaterChangeNote(){
+    return waterChangeNote;
+    }
+
+    public void changeWater(String note, WaterQuality quality){
+    this.lastWaterChange = quality;
+    this.waterChangeNote = note;
+    }
 }
