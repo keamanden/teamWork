b1615b2156143f671cea581f0e5c170b4a039aab
Raged213 <raal004@stud.kea.dk>
2025-02-26 10:34:08 +0100

    Raged Update

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,8 +1,21 @@
 public class helloWorld {
 
     public static void main(String[] args) {
+        raged();
+
+
 
         System.out.println("Hello World");
 
+
+
+    }
+    public static void raged(){
+
+        int t = 10;
+        int b = 12;
+
+
+        System.out.println(t + b);
     }
 }
